41c4ff96dd94f8ee088dd55c70c89378a77b6b64
Kevin Oliveira <kevin.zero@hotmail.com>
2021-08-03 20:36:26 -0300

    Updates

---
+++ precache-manifest.ffdccd1a1039fb3d180d62413d0e9edb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a02a55495d00e6e481fd50dc52e29e",
+    "url": "/project-frontend-online-store/index.html"
+  },
+  {
+    "revision": "9083cb590cde50fc9afe",
+    "url": "/project-frontend-online-store/static/css/main.53f10aa1.chunk.css"
+  },
+  {
+    "revision": "84e5ace7f7885a42ce69",
+    "url": "/project-frontend-online-store/static/js/2.64a88af7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/project-frontend-online-store/static/js/2.64a88af7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9083cb590cde50fc9afe",
+    "url": "/project-frontend-online-store/static/js/main.0f0842b6.chunk.js"
+  },
+  {
+    "revision": "430674a48a6c1f3060ff",
+    "url": "/project-frontend-online-store/static/js/runtime-main.411a41b2.js"
+  }
+]);
